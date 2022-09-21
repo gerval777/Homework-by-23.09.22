89d559c9257eca58cc062c33586fa4daae6ddfd7
Gerval <qwertycom21@yandex.ru>
2022-09-22 00:04:25 +0300

    DZ_4

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
-        // Задача 1
-        // А так попробовал вывести в одну строку System.out.println( dog + "     " + cat + "     " + paper);
-        var dog = 8;
-        var cat = 3.6;
-        var paper = 763789;
-        System.out.println (dog);
-        System.out.println (cat);
-        System.out.println (paper);
+        // Задача 5
+            public static void main(String[] args) {
 
-        //Задача 2
-        dog = dog + 4;
-        cat = cat + 4;
-        paper = paper + 4;
-        System.out.println (dog);
-        System.out.println (cat);
-        System.out.println (paper);
-
-        //Задача 3
-        var dogg = 8.0;
-        dogg = dogg - 3.5;
-        cat = cat - 1.6;
-        paper = paper - 7639;
-        System.out.println (dogg);
-        System.out.println (cat);
-        System.out.println (paper);
+                var friend = 19;
+                System.out.println (friend);
+                friend = friend * 2;
+                System.out.println (friend);
+                friend = friend / 7;
+                System.out.println (friend);
     }
 }
