886bf4a06cce9671029c76fd758eda8de966892f
Gerval <qwertycom21@yandex.ru>
2022-09-22 00:13:54 +0300

    DZ_6

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 public class Main {
-        // Задача 5
+        // Задача 6
             public static void main(String[] args) {
 
-                var frog = 3.5;
-                System.out.println (frog);
-                frog = frog * 10;
-                System.out.println (frog);
-                frog = frog / 3.5;
-                System.out.println (frog);
-                frog = frog + 4;
-                System.out.println (frog);
+                var firstBoxer = 78.2;
+                var secondBoxer = 82.7;
+                var weightTwoBoxers = firstBoxer + secondBoxer;
+                System.out.println ("Вес двух боксеров " + weightTwoBoxers + " кг");
+                var weightDifference = secondBoxer - firstBoxer;
+                System.out.println ("Разница в весе " + weightDifference + " кг");
+
+                var subtractionMethod = secondBoxer - firstBoxer;
+                System.out.println ("1. Метод вычитания: Разница в весе: " + subtractionMethod + " кг");
+                var divisionRemainderFunction = weightTwoBoxers % weightTwoBoxers;
+                System.out.println ("2. Метод остатка от деления: Разница в весе: " + weightDifference + " кг");
     }
 }
