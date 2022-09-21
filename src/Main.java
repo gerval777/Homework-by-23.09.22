e3d1fbe865bd86102ea5c169522b58854c54e358
Gerval <qwertycom21@yandex.ru>
2022-09-22 00:18:15 +0300

    DZ_7

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
 public class Main {
-        // Задача 6
+        // Задача 7
             public static void main(String[] args) {
 
-                var firstBoxer = 78.2;
-                var secondBoxer = 82.7;
-                var weightTwoBoxers = firstBoxer + secondBoxer;
-                System.out.println ("Вес двух боксеров " + weightTwoBoxers + " кг");
-                var weightDifference = secondBoxer - firstBoxer;
-                System.out.println ("Разница в весе " + weightDifference + " кг");
+                var totalTime =640;
+                System.out.println ("Общее время между сотрудниками: " + totalTime + " часов");
+                var oneEmployee = 8;
+                System.out.println ("Время работы каждого сотрудника составляет " + oneEmployee + " часов");
+                var totalEmployees = totalTime / oneEmployee;
+                System.out.println ("Всего работников в компании " + totalEmployees + " человек");
 
-                var subtractionMethod = secondBoxer - firstBoxer;
-                System.out.println ("1. Метод вычитания: Разница в весе: " + subtractionMethod + " кг");
-                var divisionRemainderFunction = weightTwoBoxers % weightTwoBoxers;
-                System.out.println ("2. Метод остатка от деления: Разница в весе: " + weightDifference + " кг");
+                var newEmployees = 94;
+                System.out.println ("При увеличении штата сотрудников на " + newEmployees + " человека, получается, что:");
+                var companyIncrease = totalEmployees + newEmployees;
+                System.out.println ("Общее количество нового штата сотрудников составит " + companyIncrease + " человек(а), что в свою очередь означает, что:");
+                var reallocationHours = companyIncrease * oneEmployee;
+                System.out.println ("Если в компании работает " + companyIncrease + " человек, то всего " + reallocationHours + " часов работы может быть поделено между сотрудниками");
     }
 }
